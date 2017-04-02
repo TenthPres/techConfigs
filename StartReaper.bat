c5b245b7af48d57630da652da6b9a0d6320e591f
James K <jkrrv@jkrrv.org>
2017-04-01 19:03:03 -0600

    Clarified date string
    Also added message to operator to leave computers on for pending software updates.

--- StartReaper.bat
+++ StartReaper.bat
@@ -9,7 +9,7 @@ git pull --rebase
 
 echo "Setting date variables..."
 
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a%%c)
 
 
@@ -21,3 +21,5 @@ mkdir "C:\Users\User\Google Drive\SundayServices\%mydate% %mytime%"
 echo "Starting Reaper..."
 
 Start "" "c:\Program Files\REAPER\reaper.exe" -template "C:\Users\User\Google Drive\Configurations\template.rpp" -saveas "C:\Users\User\Google Drive\SundayServices\%mydate% %mytime%\%mydate% %mytime%.rpp" -newinst
+
+msg "%username%" When you leave, please keep both computers turned on. (This time, and any other time this happy little message pops up.) --James
